bf35f5a3b1a01cb320b72a4b25a74141e6f2a025
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-15 02:43:25 +0300

    Updates

---
+++ precache-manifest.6263c69d65859d032a55e1828cfda90c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4c246d68abbc004c9a3b1b6dbf268d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62a8966c5217c1b4714b",
+    "url": "/static/css/main.a8538af8.chunk.css"
+  },
+  {
+    "revision": "85fa61e9eba4aafcaa3d",
+    "url": "/static/js/2.144b9a87.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.144b9a87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a8966c5217c1b4714b",
+    "url": "/static/js/main.5505b9b3.chunk.js"
+  },
+  {
+    "revision": "238e9a87d5746a92a4d6",
+    "url": "/static/js/runtime-main.8976ce13.js"
+  },
+  {
+    "revision": "f0ee75c96965b35021e41634a91c5ac4",
+    "url": "/static/media/bg-image-mountain.f0ee75c9.jpg"
+  }
+]);
