bde2c1452129330cd7dcd5b4af5a80c3b6827635
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-17 01:19:16 +0300

    adjusting the website to work on bigger screen size monitors. Added media query for such screens

--- src/components/navbar-components/LanguageToggler.js
+++ src/components/navbar-components/LanguageToggler.js
@@ -1,16 +1,16 @@
-import React, { useContext } from "react";
-import Context from "../../context";
+import React, { useContext } from 'react';
+import Context from '../../context';
 
 export default function LanguageToggler() {
   const { handleSwitch } = useContext(Context);
 
   return (
-    <div>
-      EN{" "}
+    <div className="language-toggler">
+      EN{' '}
       <label className="switch">
         <input type="checkbox" onChange={handleSwitch} />
         <span className="slider round"></span>
-      </label>{" "}
+      </label>{' '}
       RU
     </div>
   );
