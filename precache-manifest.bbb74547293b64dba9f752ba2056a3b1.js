21cf153701ce88eb4dbca2c975ec240e2a4fe92d
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-17 01:49:52 +0300

    Updates

--- precache-manifest.bbb74547293b64dba9f752ba2056a3b1.js
+++ precache-manifest.bbb74547293b64dba9f752ba2056a3b1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1a75a243b944542192cf323b3f6ad078",
+    "revision": "7050ad6ffc5c5e69f449c9359ffce479",
     "url": "/index.html"
   },
   {
-    "revision": "a3344c5d6f76063d01e1",
-    "url": "/static/css/main.f3a5fdc7.chunk.css"
+    "revision": "c7560e9e169cb304ba98",
+    "url": "/static/css/main.32bcf47d.chunk.css"
   },
   {
     "revision": "85fa61e9eba4aafcaa3d",
@@ -16,12 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.144b9a87.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a3344c5d6f76063d01e1",
+    "revision": "c7560e9e169cb304ba98",
     "url": "/static/js/main.de1f309a.chunk.js"
   },
   {
     "revision": "238e9a87d5746a92a4d6",
     "url": "/static/js/runtime-main.8976ce13.js"
+  },
+  {
+    "revision": "5ae8f74ef9a46fc9859d86dfd0bb43fa",
+    "url": "/static/media/bg-image-4k-screen.5ae8f74e.jpg"
   },
   {
     "revision": "f0ee75c96965b35021e41634a91c5ac4",
